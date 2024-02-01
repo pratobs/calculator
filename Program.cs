3658ba86df412e01c2813c957d82280c2c8f94fb
Tejaswi-Navuru <42056256+Tejaswi-Navuru@users.noreply.github.com>
2024-02-01 16:57:07 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("hello!");
+Console.WriteLine("Input the First Number");
+int first = int.Parse(Console.ReadLine());
+Console.WriteLine("Input the Second number");
+int second = int.Parse(Console.ReadLine());
+
+Console.WriteLine("what do you want to do with these numbers?");
+Console.WriteLine("[A]DD");
+Console.WriteLine("[S]UB");
+Console.WriteLine("[M]ULTIPLY");
+
+string userInput = Console.ReadLine();
+
+if (userInput == "A" || userInput == "a")
+{
+    Console.WriteLine(first + second);
+}
+else if (userInput == "S" || userInput == "s")
+{
+    Console.WriteLine(first - second);
+}
+else if (userInput == "M" || userInput == "m")
+{
+    Console.WriteLine(first * second);
+}
+else
+{
+    Console.WriteLine("Invalid input");
+}
+Console.ReadLine();
